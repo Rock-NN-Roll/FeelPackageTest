108fd2018e122c7c5498e3d46a0de61ae4900057
Rock <rock.zouyan@gmail.com>
2022-08-09 14:30:08 +0800

    Extra Changes

--- Assets/Scripts/LiveHUDView.cs
+++ Assets/Scripts/LiveHUDView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.UI;
 public partial class LiveHUDView : MonoBehaviour
@@ -16,10 +17,12 @@ public partial class LiveHUDView : MonoBehaviour
     [SerializeField] private float lastMaxHp;
     [SerializeField] private float lastCurHp;
     [SerializeField] private Camera Cam;
+    [SerializeField] private MMFeedbacks onDecreaseFeedbacks;
+    [SerializeField] private MMFeedbacks onIncreaseFeedbacks;
 
     public void Awake()
     {
-        Cam = Camera.main;
+        Cam = Camera.current;
     }
 
     private IEnumerator DestroyHudView()
@@ -27,9 +30,19 @@ public partial class LiveHUDView : MonoBehaviour
         yield return new WaitForEndOfFrame();
         Destroy(gameObject);
     }
+
+    public void OnMinusTen()
+    {
+        onDecreaseFeedbacks.PlayFeedbacks();
+    }
+    public void OnPlusTen()
+    {
+        onIncreaseFeedbacks.PlayFeedbacks();
+    }
+    
     protected void LateUpdate()
     {
-        transform.rotation = Cam.transform.rotation;
+        // transform.rotation = Cam.transform.rotation;
     }
     
 }
